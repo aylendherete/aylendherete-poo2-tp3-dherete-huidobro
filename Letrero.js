da8bfda7cc0bb50b3eea349964ee4c7481d51a0c
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-10-22 10:48:59 -0300

    TDD iteration

---
+++ Letrero.js
@@ -0,0 +1,6 @@
+function Letrero(){
+    this.estado=function(posicion){
+        return "Apagado";
+    }
+}
+module.exports=Letrero;
--- primerParte.test.js
+++ primerParte.test.js
@@ -1,3 +1,4 @@
+const Letrero=require('../src/Tijera');
 test("Letrero apagado", ()=>{
     const letrero=new Letrero();
     expect(letrero.estado(posicion)).toBe("Apagado";)
