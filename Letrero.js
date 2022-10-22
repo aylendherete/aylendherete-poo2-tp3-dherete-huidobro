d404d4de4fb2dd747cc8402dd79286485fc20947
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-10-22 11:19:27 -0300

    TDD iteration

--- Letrero.js
+++ Letrero.js
@@ -5,9 +5,10 @@ function Letrero(){
         return posicion.estadoActual;
     }
     this.crearLetrero=function (){
+        this.tablero=[];
         for(i=0;i<99;i++){
             for(j=0;j<99;j++){
-                this.tablero[i][j]=new Posicion([i][j]);
+                (this.tablero)[i][j]=new Posicion([i][j]);
             }
         }
     }
