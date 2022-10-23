e26db10daf6e90cd9ce86a1aee07773243c8fac1
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-10-23 18:44:08 -0300

    TDD iteration

--- Posicion.js
+++ Posicion.js
@@ -1,6 +1,15 @@
 function Posicion(posicionx,posiciony){
+    if(posicionx>99){
+        throw Error("La matriz tiene como eje x maximo 99");
+    }
+    else if(posiciony>99){
+        throw Error("La matriz tiene como eje y maximo 99");
+    }
     this.posicionx=posicionx;
     this.posiciony=posiciony;
-    this.estadoActual="Apagado";
+    this.estado="Apagado";
+    this.estadoActual=function(){
+        return this.estado;
+    }
 }
 module.exports=Posicion
