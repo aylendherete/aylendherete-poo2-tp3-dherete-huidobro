d4581f36bb9a2aaac712e9350eccb4cb85155f20
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-10-23 18:45:21 -0300

    TDD iteration

--- primerParte.test.js
+++ primerParte.test.js
@@ -1,6 +1,5 @@
 const Posicion=require('./Posicion');
 test("Letrero apagado", ()=>{
-    var posicion=new Posicion()
-
+    var posicion=new Posicion(10,10);
     expect(posicion.estadoActual()).toBe("Apagado");
 })
