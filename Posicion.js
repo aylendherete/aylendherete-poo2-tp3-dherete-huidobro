35a365ada705f217322659d67fc3d97b175eb988
aylendherete <114444320+aylendherete@users.noreply.github.com>
2022-10-23 18:49:43 -0300

    TDD iteration

--- Posicion.js
+++ Posicion.js
@@ -1,10 +1,4 @@
 function Posicion(posicionx,posiciony){
-    if(posicionx>99){
-        throw Error("La matriz tiene como eje x maximo 99");
-    }
-    else if(posiciony>99){
-        throw Error("La matriz tiene como eje y maximo 99");
-    }
     this.posicionx=posicionx;
     this.posiciony=posiciony;
     this.estado="Apagado";
--- primerParte.test.js
+++ primerParte.test.js
@@ -3,3 +3,7 @@ test("Letrero apagado", ()=>{
     var posicion=new Posicion(10,10);
     expect(posicion.estadoActual()).toBe("Apagado");
 })
+
+test("Letrero mas grande  que la matriz en eje x", ()=>{
+    expect(()=>{var posicion=new Posicion(100,10);}).toThrow(new Error("La matriz tiene como eje x maximo 99"));
+})
